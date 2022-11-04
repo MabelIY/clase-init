c6d58f59f39e573e62b4b468b76b94fc45030c11
Mabel Ana <113228105+MabelIY@users.noreply.github.com>
2022-11-04 23:37:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+class Animal:
+  def__init__(self, nombre, raza) -> None:
+    self._nombre =nombre
+    self._raza =raza
+  def saludar(saludo: str):
+    return f"{saludo}"
+    
+mi_animal = Animal("lolo", "labrador")
+print(mi_animal.saludar("guau"))
+class Perro(Animal):
+  def saludar(self, saludo: str):
+      return f'Soy {self().__nombre}{super().saludar(saludo)
+
+mi_perro = perro("toto", "dogo")
+print(mi_perro.saludar("guauuuu"))
+
+class Gato:
+  def saludo(self, saludo):
+    print(f"Mi saludo es: {saludo}")                                  
+
+mi_gato = gato()
+print(mi_gato.saludo("miau"))
+
+
+
+
+
+
+
+
+                                     
